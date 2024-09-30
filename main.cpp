440e58473edccd9d9048bcd1305a27481b824e4e
MizzenMastodon <Nbaum0701@gmail.com>
2024-09-30 19:57:09 +0000

    added comments

--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@
 using namespace std;
 
 int main() { 
+  //initializes variables given in the problem as well as a variable to store desired number of cookies
   const float SUGAR = 1.5;
   const float BUTTER = 1;
   const float FLOUR = 2.75;
   int count; 
-  
+  //asks user for number of desired cookies and stores it
   cout << "How many cookies would you like to bake?\n\n";
   cin >> count;
-  
+  //calculates the change in disired cookies from the original 48 cookies the original recepie 
   float countChange = static_cast<float>(count) / 48;
-
+  //outputs the information
   cout << "\nCookie Recipe\n" << "--------------------\n";
   cout << setprecision(3);
   cout << SUGAR * countChange << " cups of sugar\n\n";
