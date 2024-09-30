e1b7492a9ba8c3a63a42cf117790914a85d8b789
MizzenMastodon <Nbaum0701@gmail.com>
2024-09-30 19:50:21 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@ingredient-Adjuster/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() { 
+  const float SUGAR = 1.5;
+  const float BUTTER = 1;
+  const float FLOUR = 2.75;
+  int count; 
+  
+  cout << "How many cookies would you like to bake?\n\n";
+  cin >> count;
+  
+  float countChange = static_cast<float>(count) / 48;
+
+  cout << "\nCookie Recipe\n" << "--------------------\n";
+  cout << setprecision(2) << fixed;
+  cout << SUGAR * countChange << " cups of sugar\n\n";
+  cout << BUTTER * countChange << " cups of butter\n\n";
+  cout << FLOUR * countChange << " cups of flout\n\n";
+}
